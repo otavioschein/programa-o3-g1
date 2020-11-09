dff970ff9c255884edfd4c6d42d97de2819d3e26
otavioschein <otavioschein@sou.faccat.br>
2020-11-09 16:38:05 -0300

    changed main file

--- src/main/java/application/main.java
+++ src/main/java/application/main.java
@@ -20,27 +20,6 @@ public class main {
 	static Scanner reader = new Scanner(System.in);
 	 
 	public static void main(String[] args) throws ParseException{
-		
-		Date date = new Date(01/03/2000);
-		
-		Employee employee = new Employee("11", "joel", "11", "joel@");
-    	
-    	Address address = new Address("Taq", 99, "Taq", "RS");
-    	
-    	Customer customer1 = new Customer("22", "schein", "22", "schein@", date, "m", "c", Status.ACTIVE, address);
-    	
-    	Customer customer2 = new Customer("33", "otavio", "33", "otavio@", date, "m", "c", Status.ACTIVE, address);
-    	
-    	Customer customer3 = new Customer("43", "ot¡vio", "43", "otavio@", date, "m", "c", Status.INACTIVE, address);
-    	
-    	Service service1 = new Service("arrumar");
-    	
-    	EmployeeManager.employeeList.add(employee);
-    	CustomerManager.customerList.add(customer1);
-    	CustomerManager.customerList.add(customer2);
-    	CustomerManager.customerList.add(customer3);
-
-    	ServiceManager.serviceList.add(service1);
     	
 		while (option != 0) {
 			System.out.println("----------------------------------------");
